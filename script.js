fdbe3ebfac1fc4c7c5a6c3ec63b5039231b48fd5
mozop <you@example.com>
2021-11-07 21:29:00 +0600

    Price calculator done

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+// First-class increase decrease events handler
+document.getElementById('first-class-plus').addEventListener('click', function () {
+    updateTicketNumber('first-class', true);
+});
+document.getElementById('first-class-minus').addEventListener('click', function () {
+    updateTicketNumber('first-class', false);
+})
+
+
+// Economy increase decrease events handler
+document.getElementById('economy-plus').addEventListener('click', function () {
+    updateTicketNumber('economy', true);
+});
+document.getElementById('economy-minus').addEventListener('click', function () {
+    updateTicketNumber('economy', false);
+})
+
+
+//update Ticket Number
 function updateTicketNumber(ticket, isIncreasing) {
     const ticketInput = document.getElementById(ticket + '-number');
     let ticketNumber = ticketInput.value;
@@ -13,6 +32,7 @@ function updateTicketNumber(ticket, isIncreasing) {
 }
 
 
+//Get Input Value
 function getInputValue(ticket) {
     const ticketInput = document.getElementById(ticket + '-number');
     const ticketNumber = parseInt(ticketInput.value);
@@ -20,32 +40,15 @@ function getInputValue(ticket) {
 }
 
 
+//Calculate Price
 function calculateTotal() {
     const firstClassTotal = getInputValue('first-class') * 150;
     const economyTotal = getInputValue('economy') * 100;
     const subTotal = firstClassTotal + economyTotal;
     const tax = subTotal / 10;
     const totalPrice = subTotal + tax;
-    // update on the html
+
     document.getElementById('sub-total').innerText = subTotal;
     document.getElementById('tax-amount').innerText = tax;
     document.getElementById('total-price').innerText = totalPrice;
 }
-
-
-// first-class increase decrease events
-document.getElementById('first-class-plus').addEventListener('click', function () {
-    updateTicketNumber('first-class', true);
-});
-document.getElementById('first-class-minus').addEventListener('click', function () {
-    updateTicketNumber('first-class', false);
-})
-
-
-// Economy increase decrease events
-document.getElementById('economy-plus').addEventListener('click', function () {
-    updateTicketNumber('economy', true);
-});
-document.getElementById('economy-minus').addEventListener('click', function () {
-    updateTicketNumber('economy', false);
-})
